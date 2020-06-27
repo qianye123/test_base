3566eb67a1f87414822d580ef54587fe0b14b653
zhengshaoqian <zshq1114@163.com>
2020-06-27 18:13:33 +0800

    Add : add python test

--- test_python/test_python_base.py
+++ test_python/test_python_base.py
@@ -1,13 +1,62 @@
 
+#this is some tests for python
+
+
 
 tests = ['11', '23', '34', '32', '35', 354]
 
 for i in range(len(tests)):
     print(tests[i], len(tests))
-    tests.pop(i)
+
 
 
 print("***************************")
 
 for i in range(0, len(tests)):
     print(tests[i])
+print("***************************")
+
+tests = {'a': 5, 'c' : 7, 'b': 6}
+print(tests.items())
+
+for key,value in tests.items():
+    print(key + ' : ' + str(value))
+
+for key in sorted(tests.keys()):
+    print(key)
+
+for value in tests.values():
+    print(value)
+print(tests)
+
+
+print("***************************")
+
+tests_list = [1, 2, 3, 4, 5]
+tests_dict = {'name' : 'qianye', 'point' : tests_list}
+print(tests_dict)
+print(tests_dict['point'][3])
+
+for i in range(len(tests_dict['point'])):
+    print(str(i) + ':' + str(tests_dict['point'][i]))
+print("***************************")
+
+message = input('Tell me something, and i will repeat it  : ')
+print(message)
+nums = input()
+print(nums)
+print("***************************")
+
+def func_1(temp_lists):
+    temp_lists.append(12)
+    del temp_lists[2]
+
+def func_2(temp_lists):
+    temp_lists.append(12)
+    del temp_lists[2]
+
+temp_lists = [1, 2, 3, 4]
+func_2(temp_lists[:])
+print(temp_lists)
+func_1(temp_lists)
+print(temp_lists)
